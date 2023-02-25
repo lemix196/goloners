1c58498da6e8bb3e6dd8cc9b78a7a391c6a6069d
Grzegorz Lemiecha <g.lemiecha1@gmail.com>
2023-02-25 19:33:48 +0100

    add Card component with styling, add Card to App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import classes from "./App.module.css"
+import classes from "./App.module.css";
 
 import Navbar from "./components/UI/Navbar";
+import Card from "./components/UI/Card";
 
 function App() {
   return (
     <div>
       <Navbar />
-      <img className={classes['background-image']} src={require("./images/header-photo.jpg")}></img>
+
+      <img
+        className={classes["background-image"]}
+        src={require("./images/header-photo.jpg")}
+      ></img>
+      <Card>
+        <h2>Delicious meal at your door</h2>
+        <p>
+          We serve all kinds of pork knuckle, called in Poland as "golonka".
+          Pick your favourite kind, order and we will deliver it to you.
+        </p>
+        <p>
+          We use only finest and natural meat, straight from local breeders.
+        </p>
+      </Card>
     </div>
   );
 }

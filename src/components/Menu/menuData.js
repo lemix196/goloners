2ef2ca2aead1292edf8802be35f1ae7b19321732
Grzegorz Lemiecha <g.lemiecha1@gmail.com>
2023-02-26 16:47:08 +0100

    add menu dummy data

---
+++ src/components/Menu/menuData.js
@@ -0,0 +1,36 @@
+const MENU_DATA = [
+  {
+    id: "m1",
+    dishName: "Beer Stewed Knuckle",
+    description:
+      "Juicy pork knuckle stewed with Czech Pilsner and fresh vegetables.",
+    price: "13.99$",
+  },
+  {
+    id: "m2",
+    dishName: "Sauerkraut Knuckle",
+    description:
+      "Sweet pork knuckle with sour taste of sauerkraut mixed with baked potatoes.",
+    price: "16.99$",
+  },
+  {
+    id: "m3",
+    dishName: "Hetman Knuckle",
+    description:
+      "Knuckle prepared strictly to 300-year old recipe will take you to golden times of Polish nobleman",
+    price: "21.99$",
+  },
+  {
+    id: "m4",
+    dishName: "Bayern Knücklchen",
+    description:
+      "Rugged knuckle with belgian fries as a perfect snack for football match.",
+    price: "18.99$",
+  },
+  {
+    id: "m5",
+    dishName: "BBQnuckle",
+    description: "Grilled knuckle with golden BBQ glaze. Delicious",
+    price: "20.99$",
+  },
+];

32a1ee4ca9dd5af0b7b13ae06f0b9fcf51236280
Grzegorz Lemiecha <g.lemiecha1@gmail.com>
2023-03-05 20:54:51 +0100

    add showing number of ordered items at cart button

--- src/components/UI/Navbar.js
+++ src/components/UI/Navbar.js
@@ -1,16 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import classes from "./Navbar.module.css";
 import CartIcon from "./CartIcon";
+import AuthContext from "../Menu/auth-context";
 
 const Navbar = () => {
+  const ctx = useContext(AuthContext);
+
+
   return (
     <div className={classes.navbar}>
       <h2 className={classes["navbar-title"]}>Goloners</h2>
       <button className={classes["navbar-button"]}>
         <CartIcon />
         Cart
-        <span>2</span>
+        <span>{ctx.cartItemsQuantity}</span>
       </button>
     </div>
   );

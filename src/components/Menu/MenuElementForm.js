9636439ace8b30920d6e54bec4ccc79f74843232
Grzegorz Lemiecha <g.lemiecha1@gmail.com>
2023-03-05 18:08:56 +0100

    add state hook to dish form quantity field

--- src/components/Menu/MenuElementForm.js
+++ src/components/Menu/MenuElementForm.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 import styles from "./MenuElementForm.module.css";
 
 const MenuElementForm = () => {
+  const [dishQuantity, setDishQuantity] = useState("0");
+
+  const dishQuantityHandler = (event) => {
+    setDishQuantity(event.target.value);
+    console.log(dishQuantity);
+  };
+
   return (
-    <form className={styles['add-dish-form']}>
-      <label className={styles['add-dish-form-label']}>Amount</label>
-      <input className={styles['add-dish-form-input']} type="number"></input>
-      <button className={styles['add-dish-form-button']}>+ Add</button>
+    <form className={styles["add-dish-form"]}>
+      <label className={styles["add-dish-form-label"]}>Amount</label>
+      <input
+        className={styles["add-dish-form-input"]}
+        type="number"
+        value={dishQuantity}
+        onChange={dishQuantityHandler}
+      ></input>
+      <button className={styles["add-dish-form-button"]}>+ Add</button>
     </form>
   );
 };

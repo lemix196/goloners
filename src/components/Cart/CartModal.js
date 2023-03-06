08ac4fb8787d53c4cc983851f2ee46fdd36b2c8b
Grzegorz Lemiecha <g.lemiecha1@gmail.com>
2023-03-06 21:13:39 +0100

    add CartModal styling

--- src/components/Cart/CartModal.js
+++ src/components/Cart/CartModal.js
@@ -5,7 +5,14 @@ import classes from "./CartModal.module.css";
 const CartModal = (props) => {
   return (
     <div className={classes["cart-modal"]}>
-      <div className={classes["cart-modal-content"]}>{props.children}</div>
+      <div className={classes["cart-modal-window"]}>
+        <h2 className={classes["h2"]}>
+          Your order:
+          <span className={classes["cart-modal-close"]}>&times;</span>
+        </h2>
+
+        {props.children}
+      </div>
     </div>
   );
 };

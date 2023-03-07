6906d279a38764468744ddeb06ff864a1f23a613
Grzegorz Lemiecha <g.lemiecha1@gmail.com>
2023-03-07 20:28:55 +0100

    add CartElementButton styling

--- src/components/Cart/CartElementButton.js
+++ src/components/Cart/CartElementButton.js
@@ -1,9 +1,13 @@
 import React from "react";
 
+import styles from "./CartElementButton.module.css";
+
 const CartElementButton = (props) => {
-    return (
-        <button>{props.buttonText}</button>
-    )
+  return (
+    <button className={styles["cart-element-button"]}>
+      {props.buttonText}
+    </button>
+  );
 };
 
 export default CartElementButton;

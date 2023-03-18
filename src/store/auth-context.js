e25b71a35e67976cb4798fb434cebe6ad13dd018
Grzegorz Lemiecha <g.lemiecha1@gmail.com>
2023-03-18 21:38:40 +0100

    add validators preventing items cound and price to be less than 0

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -21,15 +21,21 @@ export const AuthContextProvider = (props) => {
     const sum =
       parseFloat(countCost) +
       parseInt(dishObj.quantity) * parseFloat(dishObj.price);
-    setCountCost(sum.toString());
+    if (sum < 0) {
+      setCountCost(0);
+    } else {
+      setCountCost(sum.toString());
+    }
   };
 
   const updateCartHandler = (dishObject) => {
+    // add first dish object
     if (cartList.length === 0) {
       updateCart([...cartList, dishObject]);
       return null;
     }
 
+    // change quantity if cartList contains dish object of given ID
     for (const obj of cartList) {
       if (obj.id === dishObject.id) {
         obj.quantity = (
@@ -38,6 +44,8 @@ export const AuthContextProvider = (props) => {
         return null;
       }
     }
+
+    // add new dish object to cart if cartList does not contain it
     updateCart([...cartList, dishObject]);
   };
 
@@ -47,7 +55,8 @@ export const AuthContextProvider = (props) => {
         obj.quantity = parseInt(obj.quantity) + 1;
         updateCart([...cartList]);
         setCountItems(countItems + 1);
-        setCountCost(parseFloat(countCost) + parseFloat(obj.price));
+        const summaryPrice = parseFloat(countCost) + parseFloat(obj.price);
+        setCountCost(summaryPrice);
         return null;
       }
     }
@@ -56,11 +65,38 @@ export const AuthContextProvider = (props) => {
   const dishDecreaseHandler = (dishId) => {
     for (const obj of cartList) {
       if (obj.id === dishId) {
-        obj.quantity = parseInt(obj.quantity) - 1;
-        updateCart([...cartList]);
-        setCountItems(countItems - 1);
-        setCountCost(parseFloat(countCost) - parseFloat(obj.price));
-        return null;
+        // remove element from cart if quantity decreases to 0
+        if (obj.quantity < 1) {
+          const ind = cartList.indexOf(obj);
+          updateCart(cartList.splice(ind, 1));
+          const summaryItems = countItems - 1;
+
+          if (summaryItems < 0) {
+            setCountItems(0);
+          } else {
+            setCountItems(countItems - 1);
+          }
+          const summaryPrice = parseFloat(countCost) - parseFloat(obj.price);
+
+          if (summaryPrice < 0) {
+            setCountCost(0);
+          } else {
+            setCountCost(summaryPrice);
+          }
+          return null;
+        } else {
+          obj.quantity = parseInt(obj.quantity) - 1;
+          updateCart([...cartList]);
+          setCountItems(countItems - 1);
+          const summaryPrice = parseFloat(countCost) - parseFloat(obj.price);
+
+          if (summaryPrice < 0) {
+            setCountCost(0);
+          } else {
+            setCountCost(summaryPrice);
+          }
+          return null;
+        }
       }
     }
   };
